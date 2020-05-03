9ce2bc2ab65c45d77a8d6297423ea5413a552fbf
Ananth Ganiga <ananth.ganiga@gmail.com>
2020-05-03 16:15:35 +0530

    SSIS package to Load the Actual Table

--- SQLs/createTbl_UNIT_IN_MARKET.sql
+++ SQLs/createTbl_UNIT_IN_MARKET.sql
@@ -1,7 +1,12 @@
 USE [REAL_ESTATE]
 GO
 
-/****** Object:  Table [dbo].[UNIT_IN_MARKET]    Script Date: 2/5/2020 11:26:42 AM ******/
+/****** Object:  Table [dbo].[UNIT_IN_MARKET]    Script Date: 3/5/2020 11:27:19 AM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[UNIT_IN_MARKET]') AND type in (N'U'))
+DROP TABLE [dbo].[UNIT_IN_MARKET]
+GO
+
+/****** Object:  Table [dbo].[UNIT_IN_MARKET]    Script Date: 3/5/2020 11:27:19 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -16,11 +21,11 @@ CREATE TABLE [dbo].[UNIT_IN_MARKET](
 	[TENURE] [varchar](10) NULL,
 	[TOP_] [varchar](10) NULL,
 	[AREA] [smallint] NULL,
-	[PSF] [nchar](10) NULL,
-	[AGENT_NUMBER] [varchar](8) NULL,
+	[PSF] [varchar](10) NULL,
+	[AGENT_NUMBER] [varchar](11) NULL,
 	[PAGE_NO] [smallint] NULL,
 	[SOURCE] [varchar](10) NULL,
-	[AGENT_COMMENT] [varchar](200) NULL,
+	[AGENT_COMMENT] [varchar](500) NULL,
 	[DATE_CAPTURED] [timestamp] NULL
 ) ON [PRIMARY]
 GO
